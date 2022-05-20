12a924b5273e2e12ff986567bb7c35a344630589
geckyn <geckyn@gmail.com>
2022-05-20 19:31:04 +0000

    contract: make token ERC20Mintable

--- contracts/RichToken.sol
+++ contracts/RichToken.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.5.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract Richtoken is ERC20, ERC20Detailed {
+contract Richtoken is ERC20Mintable, ERC20Detailed {
     constructor()
     ERC20Detailed("Rich", "RTK", 18) public {
-        _mint(msg.sender, 1000);
     }
 }
