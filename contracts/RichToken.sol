9618e754dbd4488f8d57edba969ee0376f8b53bb
geckyn <geckyn@gmail.com>
2022-05-21 11:32:07 +0000

    contracts: make token Pausable
    * Useful if you want to stop trades until the end of a crowdsale,
    or have an emergency switch for freezing all token transfers in the event of a large bug*

--- contracts/RichToken.sol
+++ contracts/RichToken.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 
-contract Richtoken is ERC20Mintable, ERC20Detailed {
+contract Richtoken is ERC20Mintable, ERC20Detailed, ERC20Pausable {
     constructor()
     ERC20Detailed("Rich", "RTK", 18) public {
     }
