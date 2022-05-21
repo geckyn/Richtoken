28854e266ee7e05258e739749b6097b8a6831801
geckyn <geckyn@gmail.com>
2022-05-21 18:24:18 +0000

    contracts: do not hardcode token details

--- contracts/RichToken.sol
+++ contracts/RichToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 
 contract Richtoken is ERC20Mintable, ERC20Detailed, ERC20Pausable {
-    constructor()
-    ERC20Detailed("Rich", "RTK", 18) public {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals)
+    ERC20Detailed(_name, _symbol, _decimals) public {
     }
 }
