2348c0c18066fb6d5081e4fff700675d0c0ffbe6
geckyn <geckyn@gmail.com>
2022-05-21 18:28:14 +0000

    migrations: add token details to deploy

--- migrations/2_richtoken_migration.js
+++ migrations/2_richtoken_migration.js
@@ -1,5 +1,8 @@
 const Richtoken = artifacts.require("Richtoken");
 
 module.exports = function (deployer) {
-  deployer.deploy(Richtoken);
+  const _name = "Richtoken"
+  const _symbol = "RTK"
+  const _decimals = "18"
+  deployer.deploy(Richtoken, _name, _symbol, _decimals);
 };
