90e73a127c88f50d5a2b361f0291dacd7af667e2
geckyn <geckyn@gmail.com>
2022-05-22 19:13:44 +0000

    migrations: add crowdsale deployment

---
+++ migrations/3_richcrowdsale_migration.js
@@ -0,0 +1,18 @@
+const Richtoken = artifacts.require("Richtoken");
+const RichCrowdsale = artifacts.require("RichCrowdsale");
+
+module.exports = async function (deployer, accounts) {
+  const _name = "Richtoken";
+  const _symbol = "RTK";
+  const _decimals = "18";
+
+  await deployer.deploy(Richtoken, _name, _symbol, _decimals);
+  const deployedToken = await Richtoken.deployed();
+
+  //crowdsale details
+  const rate = "200000000";
+  const wallet = "0xA47D83D9592D9Dd6d90713d76af00722c8eceed4"; //random address
+  const token = deployedToken.address;
+
+  await deployer.deploy(RichCrowdsale, rate, wallet, token);
+};
