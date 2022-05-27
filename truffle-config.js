eeeb214c4e0d1d850e0081a737f1587220bb8a67
geckyn <geckyn@gmail.com>
2022-05-27 19:59:11 +0000

    truffle: add Rinkeby testnet and configure HDWallet

--- truffle-config.js
+++ truffle-config.js
@@ -18,8 +18,9 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
+ require('dotenv').config({ path: '../secret.env'});
+ const HDWalletProvider = require('@truffle/hdwallet-provider');
+ const { INFURA_API_URL, MNEMONIC } = process.env;
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
@@ -57,14 +58,14 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    rinkeby: {
+    provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_URL),
+    network_id: 4,       // rinkeby's id
+    gas: 5500000,        // rinkeby has a lower block limit than mainnet
     // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
